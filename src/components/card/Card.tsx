96f5064c396046fc214ff065052755f6c9832284
Todor Garnev <tgarnev@abv.bg>
2020-11-08 22:42:46 +0200

    Fixed card background change issue.

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -13,16 +13,14 @@ const Card: FunctionComponent<CardProps> = ({ item, getSelectedItem }: CardProps
   const [state] = useContext(Context);
   const { backgroundType } = state;
   const { value, active, selected } = item;
-  const selectedItem = () => {
-    getSelectedItem(item);
-  }
 
   return (
-    <div className={`card${selected ? ' selected' : ''}${!active ? ' inactive' : ''}`} onClick={selectedItem}>
+    <div className={`card${selected ? ' selected' : ''}${!active ? ' inactive' : ''}`} onClick={() => getSelectedItem(item)}>
       <div className='card-inner'>
         <div className='card-front'></div>
-        <div className={`card-back ${backgroundType} ${backgroundType}-${value}`}></div>
-        {/* <div className='card-back'>{value}</div> */}
+        <div className={`card-back ${backgroundType} ${backgroundType}-${value}`}>
+          {state.backgroundType === 'numbers' && value}
+        </div>
       </div>
     </div>
   );
